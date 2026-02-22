e0df314c1d2e43dfc12dd1e643e04d6a194db3a1
Danil-Grigorev <daniil.grigorev.dev@gmail.com>
2026-02-22 19:34:02 +0100

    Exclude rust2go from dependencies on docs build
    Signed-off-by: Danil-Grigorev <daniil.grigorev.dev@gmail.com>

--- build.rs
+++ build.rs
@@ -18,26 +18,26 @@ fn should_regen(src: &Path, dst: &Path) -> Option<bool> {
 
 fn main() {
     // Allow docs.rs to build without golang dependency
-    #[cfg(docsrs)]
-    return;
-
-    // Ensure build script reruns when Rust API definitions change.
-    watch_files!("src", "go/go.sum", "go/go.mod", "go/impl.go",);
-
-    let mut builder = rust2go::Builder::new()
-        .with_go_src("go")
-        // Use dynamic linking, as we don't expect to run envtest outside of local tests use case
-        .with_link(rust2go::LinkType::Dynamic);
-
-    let src = Path::new("./src/lib.rs");
-    let dst = Path::new("./go/gen.go");
-    if let Some(true) | None = should_regen(src, dst) {
-        builder = builder.with_regen_arg(rust2go::RegenArgs {
-            src: src.display().to_string(),
-            dst: dst.display().to_string(),
-            ..Default::default()
-        });
+    #[cfg(not(docsrs))]
+    {
+        // Ensure build script reruns when Rust API definitions change.
+        watch_files!("src", "go/go.sum", "go/go.mod", "go/impl.go",);
+    
+        let mut builder = rust2go::Builder::new()
+            .with_go_src("go")
+            // Use dynamic linking, as we don't expect to run envtest outside of local tests use case
+            .with_link(rust2go::LinkType::Dynamic);
+    
+        let src = Path::new("./src/lib.rs");
+        let dst = Path::new("./go/gen.go");
+        if let Some(true) | None = should_regen(src, dst) {
+            builder = builder.with_regen_arg(rust2go::RegenArgs {
+                src: src.display().to_string(),
+                dst: dst.display().to_string(),
+                ..Default::default()
+            });
+        }
+    
+        builder.build();
     }
-
-    builder.build();
 }
